24b192485c67ebc25aa9873b61f3497a3944cd9f
tohit09Fst <tohit0333@gmail.com>
2024-03-18 18:16:49 +0530

    submit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import {Routes,Route} from 'react-router-dom'
-import Home from './components/Home';
-import Notes from './components/Notes';
-import Navbar from './components/Navbar';
+import React, { useState, useEffect } from 'react';
+import Sidebar from './components/Sidebar';
+import NoteDisplay from './components/NoteDisplay';
+import './App.css';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem('notes')) || []
+  );
+  const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: Math.random(),
+      body: ''
+    };
+    setNotes([newNote, ...notes]);
+    setActiveNote(newNote.id);
+  };
+
+  const onUpdateNote = (text) => {
+    const updatedNotes = notes.map((note) =>
+      note.id === activeNote ? { ...note, body: text } : note
+    );
+    setNotes(updatedNotes);
+  };
+
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note) => note.id !== idToDelete));
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/notes' element={<Notes/>}/>
-        <Route path='/nav' element={<Navbar/>}/>
-      </Routes>
+    <div className='App'>
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <NoteDisplay activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   );
-}
+};
 
 export default App;
